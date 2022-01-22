087deca111556b1c90865a3bb8c1bb99da49d505
LAPTOP-N1195GTQ\86442 <864427717@qq.com>
2022-01-22 23:34:56 +0800

    Document Model with config

---
+++ MyDocument.py
@@ -0,0 +1,10 @@
+from win32com.client import Dispatch
+class MyDocument:
+    
+    def __init__(self, documentFileName, range, fromPage, toPage ):
+        self.documentFileName = documentFileName
+        self.copies = 1
+        self.range = range
+        self.fromPage = fromPage
+        self.toPage = toPage
+        
