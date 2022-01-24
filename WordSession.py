1ec89a0c3827670192e1663cd933257e9e685f9e
LAPTOP-N1195GTQ\86442 <864427717@qq.com>
2022-01-24 18:01:59 +0800

    add a qt window and a select file butoon

--- WordSession.py
+++ WordSession.py
@@ -1,41 +1,56 @@
+from importlib_metadata import sys
 from win32com.client import gencache,Dispatch,constants
 import win32print
-import MyDocument
-from MyDocument import MyDocument
-app = gencache.Dispatch('Word.Application')
+from MyDocument import MyDocumentConfig
+import myqt
+from PyQt5.QtWidgets import (QWidget, QLabel,QComboBox, QApplication,QFileDialog,QPushButton) 
 
-printers = []
-fileNames = []
-
-
+class MyWindow(myqt.Example):
+    def __init__(self):
+        super().__init__()
+        self.init()
     
-mydir = 'C:\\Users\\86442\\Desktop\\TemporaryWork\\'
-testFile = mydir+'in.docx'
-docs = app.Documents
-
-docs.Open(mydir + 'in.docx')
-docs.Open(mydir + 'in2.docx')
-print(docs.Count)
-
-myDoc1 = docs.Item(2)
-myDoc2 = docs.Item(1)
-
-print(myDoc1.BuiltInDocumentProperties(constants.wdPropertyPages))
-
-app.Visible = False
-
-def printDoc( myDocument ):
-    app.PrintOut(FileName = myDocument.fileName, Range= constants.wdPrintAllDocument)
-
+    def selectFile(self):
+        fileName = QFileDialog.getOpenFileName(self, 'select a word file', 'C:\\Users\\86442\\Desktop\\TemporaryWork', 'word(*.doc *.docx)')
+        print(fileName)
+        fileNames.append(fileName)
+        
+    def init(self):
+        self.fileButton.clicked.connect(self.selectFile)
 def getPagesCount(document ):
     return document.BuiltInDocumentProperties(constants.wdPropertyPages)
 
-def setDocument(documentFileName):
+def initDocumentConfig(documentFileName):
+    docs.Open(documentFileName)
+    document = docs.Item(docs.Count)
+    toPage = getPagesCount(document)
+    myDocument = MyDocumentConfig(documentFileName=documentFileName, range=constants.wdPrintFromTo, fromPage = 1, toPage=toPage)
+    myDocs.update({ documentFileName : myDocument})
     
-    myDocument = MyDocument()
 def getAlternativePrinter():
     for it in win32print.EnumPrinters(2):
         printers.append(it[2])
+
 # printDoc(testFile)
 # print(dir(docs.Item[0].Range))
 # print(gencache.__name__)
+print(__name__)
+if __name__ == '__main__':
+    app = Dispatch('Word.Application')
+
+    printers = []
+    fileNames = []
+    myDocs = {}
+
+    mydir = 'C:\\Users\\86442\\Desktop\\TemporaryWork\\'
+    testFile = mydir+'in.docx'
+    docs = app.Documents
+    initDocumentConfig(testFile)
+    # myDocs[]
+    print(myDocs.items())
+    myDoc1 = docs.Item(1)
+    qtapp = QApplication(sys.argv)
+    ui = MyWindow()
+    ui.show()
+    sys.exit(qtapp.exec_())
+    
--- myqt.py
+++ myqt.py
@@ -1,5 +1,35 @@
-from PyQt5.QtWidgets import QApplication, QWidget
 import sys
+from tkinter import filedialog
+from PyQt5.QtWidgets import (QWidget, QLabel,
+                             QComboBox, QApplication,QFileDialog,QPushButton) 
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+ 
+    def initUI(self):
+        self.lbl = QLabel("Ubuntu", self)
+ 
+        combo = QComboBox(self)
+        combo.addItem("Ubuntu")
+        combo.addItem("Mandriva")
+        combo.addItem("Fedora")
+        combo.addItem("Arch")
+        combo.addItem("Gentoo")
+ 
+        combo.move(50, 50)
+        self.lbl.move(50, 150)
+ 
+        combo.activated[str].connect(self.onActivated)
 
-if __name__ =='__main__':
-    app = QApplication(sys.argv)
+        self.fileButton = QPushButton('select file',self)
+        self.fileButton.move(10,10)
+        
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('QComboBox')
+        self.show()
+ 
+    def onActivated(self, text):
+        self.lbl.setText(text)
+        self.lbl.adjustSize()
+    
