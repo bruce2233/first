d5849cdeb9e94b8c345daf5c3fa926617c090325
LAPTOP-N1195GTQ\86442 <864427717@qq.com>
2022-01-26 00:50:36 +0800

    set function

--- MyDocument.py
+++ MyDocument.py
@@ -1,6 +1,6 @@
 from win32com.client import Dispatch
 class MyDocumentConfig:
-    def __init__(self, documentFileName, range, fromPage, toPage ):
+    def __init__(self, documentFileName, range, fromPage, toPage):
         self.documentFileName = documentFileName
         self.copies = 1
         self.range = range
